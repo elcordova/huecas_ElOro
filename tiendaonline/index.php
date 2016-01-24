d4244eb728eeb665c440b43f1c2219268b26e5f4
elcordova <erick.lenin.03@gmail.com>
2016-01-24 11:53:10 -0500

    reestructuracion de contenidos

--- tiendaonline/index.php
+++ tiendaonline/index.php
@@ -4,22 +4,19 @@
 
 $conexion = mysqli_connect($servidor,$usuario,$contrasena,$basededatos);
 mysqli_set_charset($conexion, "utf8");
-$peticion = "SELECT * FROM productos WHERE existencias > 0";
+$peticion = "SELECT * FROM hueca ";
 $resultado = mysqli_query($conexion, $peticion);
 while($fila = mysqli_fetch_array($resultado)) {
 	echo "<article>";
-	$peticion2 = "SELECT * FROM imagenesproductos WHERE idproducto = ".$fila['id']." LIMIT 1";
-	$resultado2 = mysqli_query($conexion, $peticion2);
-	while($fila2 = mysqli_fetch_array($resultado2)) {
-		echo "<img src='photo/".$fila2['imagen']."' width=100px>";
-	}
+	echo "<img src='photo/".$fila['hue_banner']."' width=100px>";
 	
-	echo "<a href='producto.php?id=".$fila['id']."'><h3>".$fila['nombre']."</h3></a>";
-	echo "<p>".$fila['descripcion']."</p>";
-	echo "<p>Precio: ".$fila['precio']." €</p>";
+	
+	echo "<a href='hueca.php?id=".$fila['hue_id']."'><h3>".$fila['nombre']."</h3></a>";
+	echo "<p>".$fila['hue_nombre']."</p>";
+	echo "<p>Horario de Atencion: ".$fila['hue_horario']."</p>";
 	
 	echo "<br>";
-	echo "<a href='producto.php?id=".$fila['id']."'><button>Más información</button></a>";
+	echo "<a href='hueca.php?id=".$fila['hue_id']."'><button class='btn-info'>Más información</button></a>";
 	echo "<button value='".$fila['id']."' class='botoncompra'>Comprar ahora</button>";
 	echo "</article>";
 } 
