c36756f96da3c3c9b5fcd0ac15cc36e7ee64973c
elcordova <erick.lenin.03@gmail.com>
2016-01-24 13:12:44 -0500

    fuction

--- tiendaonline/index.php
+++ tiendaonline/index.php
@@ -4,21 +4,25 @@
 
 $conexion = mysqli_connect($servidor,$usuario,$contrasena,$basededatos);
 mysqli_set_charset($conexion, "utf8");
-$peticion = "SELECT * FROM hueca ";
+$peticion = "SELECT * FROM hueca WHERE hue_id=1";
 $resultado = mysqli_query($conexion, $peticion);
+
+
 while($fila = mysqli_fetch_array($resultado)) {
 	echo "<article>";
-	echo "<img src='galeria/logos/".$fila['hue_banner']."' width=100px>";
+	echo "<img src='galeria/logos/".$fila['hue_logo']."' width=100px>";
 	
 	
-	echo "<a href='hueca.php?id=".$fila['hue_id']."'><h3>".$fila['nombre']."</h3></a>";
-	echo "<p>".$fila['hue_nombre']."</p>";
+	// echo "<a href='hueca.php?id=".$fila['hue_id']."'><h3>".$fila['nombre']."</h3></a>";
+	echo "<h1>".$fila['hue_nombre']."</h1>";
 	echo "<p>Horario de Atencion: ".$fila['hue_horario']."</p>";
 	
 	echo "<br>";
-	echo "<a href='hueca.php?id=".$fila['hue_id']."'><button class='btn-info'>Más información</button></a>";
+	// echo "<a href='hueca.php?id=".$fila['hue_id']."'><button class='btn-info'>Más información</button></a>";
 	echo "</article>";
-} 
+
+	
+}
 mysqli_close($conexion);
 ?>
 
