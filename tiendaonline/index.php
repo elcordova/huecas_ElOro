1fb58474d88569e5749166b43c232c19020056db
desaguilar23 <carlos.andres.1994@hotmail.com>
2016-01-26 05:46:46 -0500

    falta
    falta

--- tiendaonline/index.php
+++ tiendaonline/index.php
@@ -14,9 +14,27 @@ while($fila = mysqli_fetch_array($resultado)) {
 	echo "<img src='galeria/logos/".$fila['hue_logo']."' width=200px   class='img-responsive'   >   ";
 	
 	echo "</div><div class='col-md-6'>";
-	 echo "<a href='hueca.php?id=".$fila['hue_id']."' class='text-center'><h3>".$fila['hue_nombre']."</h3></a>";
+	echo "<a href='hueca.php?id=".$fila['hue_id']."' class='text-center'><h3>".$fila['hue_nombre']."</h3></a>";
 
 	echo "<p>Horario de Atencion: ".$fila['hue_horario']."</p>";
+	
+	echo "<div id='cart_cerrado' class='round5 carrito-cerrado'>
+			
+			
+			<div class='add-info'>
+				 
+				<p style='color:#D06E6D; font-size: 17px; padding-top: 10px;'>Este establecimiento se encuentra cerrado.</p>
+								<div>
+					<p style='color:#EBADAC;'>ABRE EN</p>
+					<p> ".$fila['hue_abre']."</p>";
+					
+			echo "</div>";
+				
+			echo "</div>";
+			
+			echo "</div>";
+	
+	
 	echo "</div>";
 	// echo "<a href='hueca.php?id=".$fila['hue_id']."'><button class='btn-info'>Más información</button></a>";
 	
