598acebc0bfcdde45d0341aaff704aa04aed6aa1
desaguilar23 <carlos.andres.1994@hotmail.com>
2016-01-31 10:55:15 -0500

    validacion de registro
    fdfdf

--- tiendaonline/js/ajax.js
+++ tiendaonline/js/ajax.js
@@ -17,6 +17,7 @@ function objetoAjax(){
 
 
 function Registrar_Cliente(){
+var bandera="S";
 cedula = document.frmCliente.cedula.value;
 nombre = document.frmCliente.nombre.value;
 apellido = document.frmCliente.apellido.value;
@@ -24,7 +25,14 @@ direccion = document.frmCliente.direccion.value;
 telefono = document.frmCliente.telefono.value;
 correo = document.frmCliente.correo.value;
 contra = document.frmCliente.contra.value;
+ccontra = document.frmCliente.c_contra.value;
 
+
+
+
+				
+				
+if(validar(cedula,nombre,apellido,direccion,telefono,correo,contra,ccontra,bandera)){
 alert(cedula+nombre+apellido);
 alert(cedula);
 ajax = objetoAjax();
@@ -38,8 +46,129 @@ ajax.onreadystatechange=function() {
 	}
 ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 ajax.send("cedula="+cedula+"&nombre="+nombre+"&apellido="+apellido+"&direccion="+direccion+"&telefono="+telefono+"&correo="+correo+"&contra="+contra);
+}
 
 }
+
+
+function validar(cedula,nombre,apellido,direccion,telefono,correo,contra,ccontra,bandera){
+	
+	
+				var v1 = nombre;
+                var v2 = apellido;
+                var v3 = direccion;
+                var v4 = telefono;
+                var v5 = correo;
+				var v6 = contra;
+                var v7 = ccontra;
+				var v8 = cedula;
+                if( v1 == null || v1.length == 0 || /^\s+$/.test(v1) || v2 == null || v2.length == 0 || /^\s+$/.test(v2) || v3 == null || v3.length == 0 || /^\s+$/.test(v3) || v4 == null || v4.length == 0 || /^\s+$/.test(v4)  || v5 == null || v5.length == 0 || /^\s+$/.test(v5) || v6 == null || v6.length == 0 || /^\s+$/.test(v6) || v7 == null || v7.length == 0 || /^\s+$/.test(v7) || v8 == null || v8.length == 0 || /^\s+$/.test(v8)) {
+                    var box = bootbox.alert("Todos los campos son obligatorios");
+                                    box.find('.modal-content').css({ color: '#0000', 'font-size': '1.5em'});
+                                    
+                    
+					return false;
+				}
+			var espacios = false;
+			var cont = 0
+			while (!espacios && (cont < v6.length)) {
+			  if (v6.charAt(cont) == " ")
+				espacios = true;
+			  cont++;
+			}
+			 
+			if (espacios) {
+			  
+			  var box = bootbox.alert("La contraseña no puede contener espacios en blanco");
+                                    box.find('.modal-content').css({ color: '#0000', 'font-size': '1.5em'});
+			  return false;
+			}
+
+			if (v6 != v7) {
+			  var box = bootbox.alert("La contraseñas no coinciden");
+                                    box.find('.modal-content').css({ color: '#0000', 'font-size': '1.5em'});
+			  
+			  return false;
+			} 
+	
+	
+	if(cedula.length == 10){
+                    if(cedula==2222222222){
+                        var box = bootbox.alert("No es un numero de cedula");
+                                    box.find('.modal-content').css({ color: '#0000', 'font-size': '1.5em'});
+                                    box.find('.btn-primary').css({'background-color': '#33CF64','border': '#33CF64 1px solid'});
+                        return false;
+                    }
+                    //Obtenemos el digito de la region que sonlos dos primeros digitos
+                    var digito_region = cedula.substring(0,2);
+                    //Pregunto si la region existe ecuador se divide en 24 regiones
+                    if( digito_region >= 1 && digito_region <=24 ){
+                        // Extraigo el ultimo digito
+                        var ultimo_digito = cedula.substring(9,10);
+                        //Agrupo todos los pares y los sumo
+                        var pares = parseInt(cedula.substring(1,2)) + parseInt(cedula.substring(3,4)) + parseInt(cedula.substring(5,6)) + parseInt(cedula.substring(7,8));
+                        //Agrupo los impares, los multiplico por un factor de 2, si la resultante es > que 9 le restamos el 9 a la resultante
+                        var numero1 = cedula.substring(0,1);
+                        var numero1 = (numero1 * 2);
+                        if( numero1 > 9 ){ var numero1 = (numero1 - 9); }
+                        var numero3 = cedula.substring(2,3);
+                        var numero3 = (numero3 * 2);
+                        if( numero3 > 9 ){ var numero3 = (numero3 - 9); }
+                        var numero5 = cedula.substring(4,5);
+                        var numero5 = (numero5 * 2);
+                        if( numero5 > 9 ){ var numero5 = (numero5 - 9); }
+                        var numero7 = cedula.substring(6,7);
+                        var numero7 = (numero7 * 2);
+                        if( numero7 > 9 ){ var numero7 = (numero7 - 9); }
+                        var numero9 = cedula.substring(8,9);
+                        var numero9 = (numero9 * 2);
+                        if( numero9 > 9 ){ var numero9 = (numero9 - 9); }
+                        var impares = numero1 + numero3 + numero5 + numero7 + numero9;
+                        //Suma total
+                        var suma_total = (pares + impares);
+                        //extraemos el primero digito
+                        var primer_digito_suma = String(suma_total).substring(0,1);
+                        //Obtenemos la decena inmediata
+                        var decena = (parseInt(primer_digito_suma) + 1) * 10;
+                        //Obtenemos la resta de la decena inmediata - la suma_total esto nos da el digito validador
+                        var digito_validador = decena - suma_total;
+                        //Si el digito validador es = a 10 toma el valor de 0
+                        if(digito_validador == 10)
+                            var digito_validador = 0;
+                        //Validamos que el digito validador sea igual al de la cedula
+                        if(digito_validador == ultimo_digito){
+                            
+                            
+                            
+                        }else{
+                            var box = bootbox.alert("la cedula:" + cedula + " es incorrecta");
+                            box.find('.modal-content').css({ color: '#0000', 'font-size': '1.5em'});
+                            box.find('.btn-primary').css({'background-color': '#33CF64','border': '#33CF64 1px solid'});
+                            bandera="N"
+							return false;
+                        }
+                    }else{
+                        // imprimimos en consola si la region no pertenece
+                        var box = bootbox.alert("Esta cedula no pertenece a ninguna region");
+                        box.find('.modal-content').css({ color: '#0000', 'font-size': '1.5em'});
+                        box.find('.btn-primary').css({'background-color': '#33CF64','border': '#33CF64 1px solid'});
+                        bandera="N"
+						return false;
+                    }
+                }else{
+                    //imprimimos en consola si la cedula tiene mas o menos de 10 digitos
+                    var box = bootbox.alert("Esta cedula tiene menos de 10 digitos");
+                    box.find('.modal-content').css({ color: '#0000', 'font-size': '1.5em'});
+                    box.find('.btn-primary').css({'background-color': '#33CF64','border': '#33CF64 1px solid'});
+                    bandera="N"
+					return false;
+                }
+				
+				
+	return true;
+
+}
+
 
 function Ingresar_Cliente(accion){
 cedula = document.frmEquipos.cedula.value;
