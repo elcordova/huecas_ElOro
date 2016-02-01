2c8d7ceddb0e1ba34498b554adf54b4627631140
elcordova <erick.lenin.03@gmail.com>
2016-02-01 00:21:07 -0500

    comoLlegar

---
+++ tiendaonline/quienesSomos.php
@@ -0,0 +1,17 @@
+<?php include "php/cabecera.inc" ?>
+<?php include "php/config.inc" ?>
+
+
+<th>
+<div class="panel col-xs-12 container-fluid text-center">
+<div class="page-header">
+  <h1> QUIENES SOMOS <small> OROHUECAS </small></h1>
+</div>
+<h3>El portal OroHuecas.com permite realizar pedidos en la mayoria de los locales de comida mas reconicidos de la provincia de El Oro </h3>
+<p></p>
+</div>
+
+
+
+
+<?php include "php/piedepagina.inc" ?>
